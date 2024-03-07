09638990967230fe2cbf0654f4d58afc0e8f95e3
Mac <mac@Macs-MacBook-Pro.local>
2024-03-07 13:11:07 +0700

    update on firebase authentication for create accounts

---
+++ app/src/main/java/kh/edu/rupp/madproject/fragments/AccountOptions.kt
@@ -0,0 +1,7 @@
+package kh.edu.rupp.madproject.fragments
+
+import androidx.fragment.app.Fragment
+import kh.edu.rupp.madproject.R
+
+class AccountOptions: Fragment(R.layout.fragment_account_options) {
+}
